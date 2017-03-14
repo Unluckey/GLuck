666e0aa75f85235b8ad546aa53c5610a662940d8
Unluckey <diczero@126.com>
2017-03-14 10:23:16 +0800

    Complete the vector2 class & Add a basic collision detection

--- GLuckObject.h
+++ GLuckObject.h
@@ -10,16 +10,60 @@
 #include <vector>
 using namespace std;
 
-class Vector2
+
+class GLuckVector2
 {
 public:
-	float x, y;
-	Vector2(float iniX, float iniY);
+	GLuckVector2(float iniX, float iniY);
+
+	void set(float x, float y);
+	void setX(float x) {vecX = x; update();};
+	void setY(float y) { vecY = y; update();};
+	void addX(float x) { vecX += x; update();};
+	void addY(float y) { vecY += y; update();};
+
+	float getX() { return vecX; };
+	float getY() { return vecY; };
+	float getSlope() { return slope; };
+	int getQuad() { return (int)quadrant; };
+protected:
+	void update();
+	enum direction{RightUp, LeftUp,LeftDown,RightDown}quadrant;
+	float slope;
+	float vecX, vecY;
+	
 };
 
-Vector2::Vector2(float iniX = 0, float iniY = 0) {
-	x = iniX;
-	y = iniY;
+inline GLuckVector2::GLuckVector2(float iniX = 0, float iniY = 0) {
+	vecX = iniX;
+	vecY = iniY;
+	update();
+}
+
+inline void GLuckVector2::update()
+{
+	if (vecX != 0)
+		slope = vecY / vecX;
+	else
+		slope = 0;
+
+	if (vecX >= 0)
+		if (vecY > 0)
+			quadrant = (direction)1;
+		else
+			quadrant = (direction)4;
+	else
+		if (vecY > 0)
+			quadrant = (direction)2;
+		else
+			quadrant = (direction)3;
+}
+
+inline void GLuckVector2::set(float x, float y)
+{
+	vecX = x;
+	vecY = y;
+	update();
 }
 
 
@@ -69,11 +113,11 @@ class GLuckObject
 {
 public:
 	GLuckObject(GLfloat iniPosX, GLfloat iniPosY, bool iniGravity);
-	~GLuckObject();
+	~GLuckObject() {};
 	bool setGravity(bool newGravity);
 	bool changeGravity();
-	GLfloat posX();
-	GLfloat posY();
+	GLfloat posX() { return x; };
+	GLfloat posY() { return y; };
 	int ID();
 
 	void setColor(int r, int g, int b);
@@ -82,31 +126,25 @@ public:
 	virtual bool move();
 	virtual void transform(GLfloat posX, GLfloat posY);
 	virtual void display() {};
-
+	GLuckVector2 speed;
 
 protected:
 	float elastic;
-	Vector2 speed;
+	
 	GLuckColor RGB;
 	GLfloat x,y;
 	bool onGravity;
 	int ObjID;
+	int mass;
 };
 
 GLuckObject::GLuckObject(GLfloat iniPosX = 0, GLfloat iniPosY = 0, bool iniGravity = false) {
-	speed.x = 0;
-	speed.y = 0;
 	x = iniPosX;
 	y = iniPosY;
 	onGravity = iniGravity;
-	//ObjID = ++NumOfObj;
 	setColor(255,255,255);
 	elastic = 0.4;
-}
-
-inline GLuckObject::~GLuckObject()
-{
-
+	mass = 1;
 }
 
 bool GLuckObject::setGravity(bool newGravity){
@@ -119,14 +157,6 @@ inline bool GLuckObject::changeGravity()
 {
 	onGravity = !onGravity;
 	return onGravity;
-}
-
-GLfloat GLuckObject::posX() {
-	return x;
-}
-
-GLfloat GLuckObject::posY() {
-	return y;
 }
 
 inline int GLuckObject::ID()
@@ -147,14 +177,14 @@ inline void GLuckObject::setColor(GLuckColor color)
 }
 
 bool GLuckObject::move(){
-	x += speed.x;
-	y -= speed.y;
-	if (onGravity && (int)y > 0)speed.y += GRAVITY;
+	x += speed.getX();
+	y -= speed.getY();
+	if (onGravity && (int)y > 0)speed.addY(GRAVITY);
 
 	if (y < 0) {
 		y = 0;
-		speed.y = (-speed.y + 1) * elastic;
-		if (speed.y > -1)speed.y = 0;
+		speed.setY((-speed.getY() + 1) * elastic);
+		if (speed.getY() > -1)speed.setY(0);
 	}
 	return onGravity;
 }
@@ -177,9 +207,12 @@ public:
 	GLfloat scale(GLfloat changeValue, GLfloat maxScaleSzie);
 	virtual void rotate(GLfloat speed);
 	virtual void display();
+	//added
+	GLuckVector2 force;
+	bool collDetection(GLuckPolygon other);
 
 protected:
-	void refresh();
+	void update();
 	bool clockWise;
 	GLfloat rotationAngle;
 	vector<GLuckPOINT> vPivot;
@@ -203,23 +236,51 @@ GLuckPolygon::GLuckPolygon(int iniEdge,GLfloat iniRange, int iniPosX = 0, int in
 	}
 }
 
-void GLuckPolygon::refresh() {
-	float dAng = 2 * PI / edge;
-	GLfloat minY = vPivot.at(0).y;
-	int minYPivot = 0;
-	for (int i = 0; i < edge; i++)
-		vPivot.at(i).set(x + range*sin(dAng*i + rotationAngle), y + range*cos(dAng*i + rotationAngle));
+inline bool GLuckPolygon::collDetection(GLuckPolygon other)
+{
+	GLfloat distance = pow(this->posX() - other.posX(), 2) + pow(this->posY() - other.posY(),2);
+	if (distance > range)
+		return false;
+	else
+	{
+
+		return true;
+	}
 }
 
+inline GLfloat GLuckPolygon::scale(GLfloat changeValue, GLfloat maxSzie)
+{
+	range += changeValue;
+	if (range <= 0) range = 0;
+	if (range > maxSzie)range = maxSzie;
+	update();
+
+	return range;
+}
+
+inline void GLuckPolygon::rotate(GLfloat speed)
+{
+	if (!clockWise) {
+		rotationAngle += speed;
+		rotationAngle = rotationAngle > PI * 2 ? 0 : rotationAngle;
+	}
+	else {
+		rotationAngle -= speed;
+		rotationAngle = rotationAngle < -PI * 2 ? 0 : rotationAngle;
+	}
+}
+
+
+
 inline bool GLuckPolygon::move() {
-	x += speed.x;
-	y -= speed.y;
-	if (onGravity && (int)y > range)speed.y += GRAVITY;
+	x += speed.getX();
+	y -= speed.getY();
+	if (onGravity && (int)y > range)speed.addY(GRAVITY);
 	//bounce
 	if (y < range) {
 		y = range;
-		speed.y = (-speed.y + 1) * elastic;
-		if (speed.y > -1)speed.y = 0;
+		speed.setY((-speed.getY() + 1) * elastic);
+		if (speed.getY() > -1)speed.setY(0);
 	}
 	return onGravity;
 }
@@ -227,7 +288,7 @@ inline bool GLuckPolygon::move() {
 inline void GLuckPolygon::display()
 {
 	move();
-	refresh();
+	update();
 	glColor3ub(RGB.r,RGB.g,RGB.b);
 	
 	glBegin(GL_POLYGON);
@@ -239,27 +300,14 @@ inline void GLuckPolygon::display()
 	
 }
 
-inline GLfloat GLuckPolygon::scale(GLfloat changeValue,GLfloat maxSzie)
-{
-	range += changeValue;
-	if (range <= 0) range = 0;
-	if (range > maxSzie)range = maxSzie;
-	refresh();
-	
-	return range;
+void GLuckPolygon::update() {
+	float dAng = 2 * PI / edge;
+	GLfloat minY = vPivot.at(0).y;
+	int minYPivot = 0;
+	for (int i = 0; i < edge; i++)
+		vPivot.at(i).set(x + range*sin(dAng*i + rotationAngle), y + range*cos(dAng*i + rotationAngle));
 }
+ 
 
-inline void GLuckPolygon::rotate(GLfloat speed)
-{
-	if (!clockWise) {
-		rotationAngle += speed;
-		rotationAngle = rotationAngle > PI*2 ? 0 : rotationAngle;
-	}
-	else{
-		rotationAngle -= speed;
-		rotationAngle = rotationAngle < -PI*2 ? 0 : rotationAngle;
-	}
-}
-
-
-std::vector<GLuckPolygon> vPolygons;
+//Object Manager vector
+vector<GLuckPolygon> vPolygons;
